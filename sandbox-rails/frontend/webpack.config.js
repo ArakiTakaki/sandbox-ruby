2be692b3b8658fd8579cdb09e6b82aa1bd8621fd
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-02-02 16:12:12 +0900

    最低限のセッティング

---
+++ sandbox-rails/frontend/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: ['./src/ts/index.tsx'],
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				use: ['ts-loader']
+			},
+			{
+				enforce: 'pre',
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				loader: 'tslint-loader'
+			}
+		]
+	},
+	devtool: 'inline-source-map',
+	output: {
+		path: path.resolve(__dirname, './dist/js'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	devServer: {
+		contentBase: '/dist/js',
+		port: 3030,
+		// 他デバイスで見たい時用
+		host: '0.0.0.0',
+		overlay: true,
+		filename: 'bundle.js',
+		publicPath: '/',
+		stats: {
+			colors: true
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			hash: true,
+			template: path.resolve(__dirname, './src/html/index.html')
+		})
+	]
+};
