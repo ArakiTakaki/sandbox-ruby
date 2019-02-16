772eacf577d4841f93c0262426e9492caa0ebe66
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-02-16 15:21:14 +0900

    corsを変更

--- sandbox-rails/config/initializers/cors.rb
+++ sandbox-rails/config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "localhost:3030"
 
-    resource "*",
+    resource "localhost:3030",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       expose: ['Per-Page', 'Total', 'Link']
