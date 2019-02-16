c1e2fecb6eb7b7324840f151c0d217d383fad51d
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-02-16 14:16:07 +0900

    develop用のwebpackの設定を完備

--- sandbox-rails/frontend/webpack.config.js
+++ sandbox-rails/frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isSourceMap = true;
 
 module.exports = {
 	mode: 'development',
@@ -12,10 +13,36 @@ module.exports = {
 				use: ['ts-loader']
 			},
 			{
-				enforce: 'pre',
-				test: /\.(ts|tsx)$/,
-				exclude: /node_modules/,
-				loader: 'tslint-loader'
+				test: /\.sass$/,
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: isSourceMap,
+							localIdentName: '[name]-[local]-[hash:base64:4]',
+							modules: true
+						}
+					},
+					{
+						loader: 'postcss-loader',
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: isSourceMap
+						}
+					},
+					{
+						loader: 'sass-resources-loader',
+						options: {
+							sourceMap: isSourceMap,
+							resources: [path.resolve('./src/scss/resources/*.scss')]
+						}
+					}
+				]
 			}
 		]
 	},
@@ -27,8 +54,11 @@ module.exports = {
 	},
 	devServer: {
 		contentBase: '/dist/js',
+		disableHostCheck: true,
 		port: 3030,
+		// 他デバイスで見たい時用
 		host: '0.0.0.0',
+		hot: true,
 		overlay: true,
 		filename: 'bundle.js',
 		publicPath: '/',
